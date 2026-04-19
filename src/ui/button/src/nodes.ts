84547b6746aed52303818e80282c4ed9804a2eed
goshasafonov <goshasafonov@yandex.ru>
2026-04-19 18:24:16 +0300

    added cursor, before

--- src/ui/button/src/nodes.ts
+++ src/ui/button/src/nodes.ts
@@ -1,7 +1,19 @@
 import type { ButtonUI } from "../../../components/Button/src/types/ButtonUI";
 
 export const nodes = {
-    root: ['d-inline-flex', 'position-relative', 'gap-1'],
+    root: [
+        'd-inline-flex',
+        'cursor-pointer',
+        'position-relative',
+        'gap-1',
+        'before:inset-0',
+        'before:opacity-0',
+        'before:bg-current',
+        'before:transition',
+        'before:rounded-inherit',
+        'hover:before:opacity-10',
+        'before:position-absolute',
+    ],
     icon: {
         append: ['d-inline-flex'],
         prepend: ['d-inline-flex'],
